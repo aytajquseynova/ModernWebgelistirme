34c10fefdc433c050a4dbbebbaefb5f32d865c2a
aytajquseynova <you@example.com>
2023-09-24 23:16:39 +0400

    put ve delete

---
+++ AsenkronProgramlamaPart2FetchPromise/fetchvePromiseIlePutveDelete/app.js
@@ -0,0 +1,61 @@
+class Request {
+    get(url) { //get request
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(err => reject(err))
+        })
+    }
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: 'POST',
+                    body: JSON.stringify(data),
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    },
+                })
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+
+        })
+
+
+    }
+    put(url, data){
+        return new Promise((resolve, reject) => {
+        fetch(url, {
+                method: 'PUT',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+        
+
+    }
+    delete(url){
+        return new Promise((resolve, reject )=> { 
+        fetch('https://jsonplaceholder.typicode.com/albums/1', {
+        method: 'DELETE',
+        }).then(response => resolve ("veri silme islemi basarili"))
+        .catch(err => reject(err));
+    })
+
+    };
+}
+// https://jsonplaceholder.typicode.com/albums
+
+const request = new Request();
+// request.put("https://jsonplaceholder.typicode.com/albums/1", {userId:10, title :"Tarkan Karma"})
+// .then(album => console.log(album))
+// .catch(err => console.log(err))
+request.delete("https://jsonplaceholder.typicode.com/albums/1")
+.then(message => console.log(message))
+.catch(err => console.log(err));
